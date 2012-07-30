2a280df13c563d579f541529d9b6946f49fd6b6d
John <thesobriquet@gmail.com>
2012-07-30 16:44:18 +0800

    addded System.out for tracking connectedIP state

--- bcmBackend/IPList.java
+++ bcmBackend/IPList.java
@@ -6,15 +6,30 @@ public class IPList extends LinkedList<String> {
 
 	public String find(String ipToSearch) {
 		for (String k: this) {
-			if (ipToSearch.equals(k)) return k;
+			if (ipToSearch.equals(k)) {
+				System.out.println(k + " found");
+				return k;
+				}
 		}
 		return null;
 	}
 	
 	public String removeIP(String ipToRemove) {
 		String ipt = this.find(ipToRemove);
+		System.out.println("before deleting: " + this);
 		if (ipt != null) { this.remove(ipt); }
+		System.out.println("after deleting: " + this);
 		return ipt;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (String k: this) {
+			sb.append(k + "  ");
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 	
 	
